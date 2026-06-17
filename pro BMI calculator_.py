9ce06150b6f8d424bdb3ac05c4b20e23d8dda79a
sainoooo <sainashoja@gmail.com>
2026-06-17 11:33:03 +0330

    Update pro BMI calculator_.py

--- pro BMI calculator_.py
+++ pro BMI calculator_.py
@@ -4,18 +4,15 @@ hieght = float(input("enter your hieght(m): "))
 BMI = weight/ hieght**2
 if BMI<=18.5:
     massage = ("underweight")
-elif 18.5 < BMI <= 25:
+elif BMI <= 25:
     massage = ("normal weight")
-elif 25 < BMI <= 30:
+elif BMI <= 30:
     massage = ("overweght")
-elif 30 < BMI <= 35:
+elif BMI <= 35:
     massage = ("obese")
 else:
     massage = ("clinically obese")
 newBMI = "{:.2f}" .format(BMI)
 print(f"your BMI is {newBMI} you are {massage}")
-
-
-
 
 
